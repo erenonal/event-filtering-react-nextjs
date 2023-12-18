86c09108c9db1d8688e7cf9abd41d6acb7c39ff9
Eren Önal <75484033+erenonal@users.noreply.github.com>
2023-12-19 01:31:42 +0300

    SSG added

--- components/icons/arrow-right-icon.js
+++ components/icons/arrow-right-icon.js
@@ -3,7 +3,7 @@ function ArrowRightIcon() {
     <svg
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 20 20'
-      fill='currentColor'
+      fill='#ececec'
     >
       <path
         fillRule='evenodd'
