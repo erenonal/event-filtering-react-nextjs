4218d75ee6948f0cf2ea9fbae985762accc03880
Eren Önal <75484033+erenonal@users.noreply.github.com>
2023-12-19 12:51:07 +0300

    ssr is used for dynamic filtered page events from firabase db

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,6 +1,6 @@
 export async function getAllEvents() {
   const response = await fetch(
-    'https://lotr-event-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+    "https://lotr-event-default-rtdb.europe-west1.firebasedatabase.app/events.json"
   );
   const data = await response.json();
   const events = [];
@@ -17,4 +17,18 @@ export async function getFeaturedEvents() {
 export async function getEventById(id) {
   const allEvents = await getAllEvents();
   return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
 }
